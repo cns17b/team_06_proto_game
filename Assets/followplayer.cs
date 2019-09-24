6df46c704c134dcd81363095fb67888a8aca2bd2
cns17b <cns17b@my.fsu.edu>
2019-09-24 10:59:30 -0400

    Player movement added.  Camera not working properly

---
+++ Assets/followplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+    public float distance = -6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position.z = player.position.z + distance;
+    }
+}
