604b00f0f13bd9a1defccf1c72f4dd295f72434e
cns17b <cns17b@my.fsu.edu>
2019-10-21 19:05:35 -0400

    Attempt to rebase

--- Assets/Scripts/playercontrol.cs
+++ Assets/Scripts/playercontrol.cs
@@ -4,127 +4,88 @@ using UnityEngine;
 
 public class playercontrol : MonoBehaviour
 {
-    public GameObject PlayerProjectile;
+
     public float speed;
-    public float invincibletimer;
-    public int hit;
+    private float health;
+    private bool hasShield;
+    private float ammo;
     Rigidbody2D rb;
     AudioSource audioData;
-    public Animator anim;
-    public GameObject health1;
-    public GameObject health2;
-    public GameObject health3;
-
     // Start is called before the first frame update
     void Start()
     {
-        hit = 0;
+        ammo = 15;
+        hasShield = false;
+        health = 3;
         rb = GetComponent<Rigidbody2D>();
         audioData = GetComponent<AudioSource>();
         speed = 30f;
-        invincibletimer = 0;
-
-        anim.SetBool("isMoving", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        //Play Idle animation, and moving animation if moving
-        invincibletimer = invincibletimer - Time.deltaTime;
-
         float move = Input.GetAxis("Vertical");
-
-        if (move > 0)
+        rb.velocity = new Vector2(rb.velocity.x, speed * move);
+        //End game if health is zero
+        if (health == 0)
         {
-            //Debug.Log("pos");
-            rb.velocity = new Vector2(rb.velocity.x, speed * move);
-            anim.SetBool("isMoving", true);
+            //play death animation
+            Time.timeScale = 0;
         }
-        else if (move < 0)
-        {
-            //Debug.Log("neg");
-            rb.velocity = new Vector2(rb.velocity.x, speed * move);
-            anim.SetBool("isMoving", true);
-        }
-        else if (move == 0)
-        {
-            anim.SetBool("isMoving", false);
-
-        }
-        if (hit == 3)
-        {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
-            anim.SetBool("Dead", true);
-            if (invincibletimer <= 0)
-            {
-                endgame();
-            }
-        } 
-        //Fire Projectile is Space is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger("shoot");
-            shoot();
-        }
+        //Add code to shoot when player presses space button if they have ammo
+        //Add code to speed up and slow down with left and right
+        //Add code to spawn ammo is the player does not have any ammo remaining for more than 15 seconds.
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (invincibletimer <= 0)
+        //Play hit sound and move back if hit by meteor or projectile
+        if (col.gameObject.tag == "Meteor")
         {
-            //Play hit sound and hit animation
-            if (col.gameObject.tag == "Meteor")
+            if (hasShield == false)
             {
-                //Play Heath Loss animation (UI)
-                hit = hit + 1;
-                invincibletimer = 3;
+                transform.Translate(0, 2, 0);
                 audioData.Play(0);
+                health = health - 1;
+                //play hit animation
+                //Update heart display
             }
-            if (col.gameObject.tag == "Enemy")
+            else
             {
-                //Play Heath Loss animation (UI)
-                hit = hit + 1;
-                invincibletimer = 3;
-                audioData.Play(0);
+                hasShield = false;
+                //stop shield animation
             }
-            if (col.gameObject.tag == "EnemyProjectile")
-            {
-                //Play Heath Loss animation (UI)
-                hit = hit + 1;
-                invincibletimer = 3;
-                audioData.Play(0);
-            }
-        }
-        if (hit == 1)
-        {
-            Destroy(health1);
-        }
-        if (hit == 2)
-        {
-            Destroy(health2);
-        }
-        if (hit == 3)
-        {
-            Destroy(health3);
         }
 
-    }
-    void shoot()
-    {
-        Instantiate(PlayerProjectile, transform.position, transform.rotation);
-    }
-    void endgame()
-    {
-      
-       Time.timeScale = 0;
-       Destroy(this.gameObject);
-       
+        // Give a shield if shield is picked up
+        if (col.gameObject.tag == "shield")
+        {
+            hasShield = true;
+            //Play shield animation
+        }
+        //Gain health if health is picked up (max 3)
+        if (col.gameObject.tag == "health")
+        {
+            if (health < 3)
+            {
+                health = health + 1;
+                //Update heart display
+            }
+        }
 
+        //Gain ammo is ammo is picked up (max 15)
+        if (col.gameObject.tag == "ammo")
+        {
+            ammo = 15;
+            //Update UI.
+        }
     }
+
+    
 }
